01613d5e6fa32c9d9c3026d33e5e0444ef73becd
sa-GT <habeeb.alafeef6@gmail.com>
2025-01-02 19:10:35 +0300

    sss

---
+++ function.js
@@ -0,0 +1,237 @@
+// Q1
+// function tellFortune(children,partner,geographic,job_title){
+//     console.log("You will be a"+ " " +job_title+" " + "in"+" " + geographic + " " + "married to" + " "+ partner+ " " + "with"+" " + children + "kids"); 
+// }
+
+// tellFortune(5,"alice","amman","software")
+// --------------------------------------------------------------------------
+//Q2
+// function calculateDogAge(puppyage){
+//     let resultt=puppyage * 7
+//     return "Your puppy age is" +" "+resultt 
+// }
+
+// console.log(calculateDogAge(2))
+// ------------------------------------------------------------------------------
+// Q3
+// function calculateSupply(age,amount){
+//     let cop = amount * 365
+//     return (100 - age) * cop
+// }
+// console.log(calculateSupply(30,3));
+// ---------------------------------------------
+// Q4
+// function greet(name){
+//     return "hello" +" "+ name
+// }
+// console.log(greet("sameh"));
+// ------------------------------------------------------------
+//Q5
+// function double(cat) {
+//     return 2 * x;
+// }
+
+// function double(cat) {
+//     return 2 * 7;
+// }
+
+// function double(cat) {
+//     let x = cat
+//     return 2 * 'x';
+// }
+// ----------------------------------------------------------------
+//Q6
+// function double1(x) {
+//     return 2 * x 
+//   }
+//   function double2 (x){
+//   return 2 * x;
+//   }
+//   function double3(x) {
+//     return 2 * x
+// }
+// --------------------------------------------------------------
+//Q7
+// function cube(num){
+//     return num**3
+// }
+// console.log(cube(4));
+// ---------------------------------------------------------------
+// // Q8
+// function multiply(num1,num2){
+//     return num1 * num2
+// }
+// console.log(multiply(3,4));
+// ---------------------------------------------------------------
+// Q9
+// function canIGetADrivingLicense(age){
+//     let age_restrect= 20 - age
+//     if (age >=20){
+//         return "you can"
+//     }
+//     else{
+//         return "Come back later after" + " " + age_restrect 
+//     }
+// }
+// console.log(canIGetADrivingLicense(17))
+//--------------------------------------------------------------
+//Q10
+// function  sameLength(s1,s2){
+//     if(s1.length == s2.length){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(sameLength("hello","hello"))
+// -------------------------------------------------------------------
+//Q11
+// function largerNubmer(num1,num2){
+//     if(num1>num2){
+//         return "num1 larger than num2"+ " " + num1 
+//     }
+//     else{
+//         return "num2 larger than num1" + " " + num2
+//     }
+// }
+
+// console.log(largerNubmer(4,20));
+//------------------------------------------------------------------
+//Q12
+// function smallerNubmer(num1,num2,num3){
+//     if(num1 < num2 && num1 < num3){
+//         return num1
+//     }else if (num2 < num1 && num2 < num3){
+//         return num2
+//     }else{
+//         return num3
+//     }
+// }
+
+// console.log(smallerNubmer(4,2,-1));
+//------------------------------------------------------
+//Q13
+// function shorterString(string1,string2,string3){
+//     if(string1.length < string2.length && string1.length < string3.length){
+//         return string1
+//     }else if(string2.length < string1.length && string2.length < string3.length){
+//         return string2
+//     }else{
+//         return string3
+//     }
+// }
+// console.log(shorterString("hello","wellcome","ss"))
+// --------------------------------------------------------------
+//Q14
+// function longerString(string1,string2,string3){
+//     if(string1.length > string2.length && string1.length > string3.length){
+//         return string1
+//     }else if(string2.length > string1.length && string2.length > string3.length){
+//         return string2
+//     }else{
+//         return string3
+//     }
+// }
+// console.log(longerString("hello","wellcome","ss"))
+// ------------------------------------------------------------------
+//Q15
+// function isEven(num1){
+//     if(num1%2==0){
+//         return true
+//     }
+// }
+// console.log(isEven(4))
+// -------------------------------------------------------------
+//Q16
+// function isodd(num1){
+//     if(num1%2==1){
+//         return "is odd" +" "+ true
+//     }
+// }
+// console.log(isodd(3))
+//------------------------------------------------------------------
+//Q17
+// function positive(num1){
+//     return Math.abs(num1)
+// }
+// console.log(positive(-20))
+// function positive(num1) {
+//   if(num1 < 0){
+//     return -num1
+//   }
+//   else{
+//     num1
+//   }
+// }
+// console.log(positive(-20));
+// -----------------------------------------------------
+//Q18
+// function Fullname(fname,lname){
+//     return "your fullname name" + " " + fname + " " +lname
+// }
+// console.log(Fullname("ahmmad","saad"))
+// Q19
+// function averagee(n1,n2,n3,n4,n5){
+//     var arrayss=[n1,n2,n3,n4,n5]
+//     let lens=arrayss.length
+//     let results = 0
+//     for(let i = 0 ; i < arrayss.length ; i++ ){
+//         results+=arrayss[i]
+//     }
+//     let finalresult = results / lens
+//     console.log(finalresult);
+// }
+// averagee(4,5,6,8,6)
+//---------------------------------------------------
+//Q20
+// function random(){
+//     console.log(Math.random())
+// }
+// random()
+//-------------------------------------------------------------
+//Q21
+// function random(max,min){
+//     console.log(Math.random() +" "+ 7)
+// }
+// random(6,1)
+// -------------------------------------------
+//Q22
+// function scoreinuniversity(g){
+//     if(0<=g && g <= 49){
+//         console.log("fail");
+//     }else if(50<=g && g <= 69){
+//         console.log("D");
+//     }else if (70<=g && g <= 84){
+//         console.log("C");
+//     }else if(85<=g && g <= 94){
+//         console.log("B");
+//     }else{
+//         console.log("A");
+//     }
+// }
+// scoreinuniversity(100)
+//---------------------------------------------------------------------------------
+//Q23
+// let currentCount = 0;
+
+// function counter() {
+//     return currentCount++;
+// }
+//-----------------------------------------------
+//Q24
+// function resetCounter() {
+//     let lastCount = currentCount - 1;
+//     currentCount = 0;
+//     return lastCount+' '+'and the counter reset now';
+// }
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(resetCounter());
+// console.log(counter());
+// console.log(counter());
+// console.log(resetCounter());
+
+
+
